3e3584a2ee1196425cef9c3413d0f02e217811d0
PST-77 <20090626@alumno.utmetropolitana.edu.mx>
2023-01-16 01:31:15 -0600

    Building Layout
    SE sube la practica de Buildind Layout

---
+++ flutter_layaout/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_layaout/buttonsections.dart';
+import 'package:flutter_layaout/textsections.dart';
+import 'package:flutter_layaout/titlesection.dart';
+
+void main() => runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          
+          elevation: 0,
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column (
+            children:  [ 
+            Image.asset(
+              'assets/lake.jpg',
+              width: 600,
+              height: 240,
+              fit:BoxFit.cover),
+            titleSection(),
+            
+            buttonsection(),
+            textsections(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_layaout/lib/titlesection.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class titleSection extends StatelessWidget {
+  const titleSection({super.key});
+
+  
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: [
+      Expanded(
+        /*1*/
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /*2*/
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: const Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              'Kandersteg, Switzerland',
+              style: TextStyle(
+                color: Color.fromARGB(255, 196, 45, 45),
+              ),
+            ),
+          ],
+        ),
+      ),
+      /*3*/
+      Icon(
+        Icons.star,
+        color: Colors.red[500],
+      ),
+      const Text('41'),
+    ],
+  ),
+);
+    
+  }
+}
